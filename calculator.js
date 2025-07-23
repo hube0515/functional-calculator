09b22d7b7de7a39aa9821bcb8ed81975ac32afc7
hube0515 <hrbence11@gmail.com>
2025-07-23 11:32:02 +0200

    completed

--- calculator.js
+++ calculator.js
@@ -1,15 +1,23 @@
 import promptSync from "prompt-sync";
 const prompt = promptSync({ sigint: true });
+console.log("Hello this is the calculator!");
 
 function askOperand() {
-  let leftOperand = Number(prompt("Please give the first number"));
-  while (isNaN(leftOperand)) {
-    leftOperand = Number(prompt("Please give a valid number."));
+  let leftOperand = prompt("Please give the first number or just quit");
+  if (leftOperand === "quit") {
+    process.exit(0);
   }
+  while (isNaN(leftOperand)) {
+    leftOperand = prompt("Please give a valid number.");
+  }
+
   return leftOperand;
 }
 function askOperand2() {
-  let rightOperand = Number(prompt("Please give the second number"));
+  let rightOperand = prompt("Please give the second number or just quit");
+  if (rightOperand === "quit") {
+    process.exit(0);
+  }
   while (isNaN(rightOperand)) {
     rightOperand = Number(prompt("Please give a valid number."));
   }
@@ -17,7 +25,10 @@ function askOperand2() {
 }
 
 function askOperator() {
-  let operator = prompt("Please give a operator(+,-,*,/)");
+  let operator = prompt("Please give a operator(+,-,*,/) or just quit");
+  if (operator === "quit") {
+    process.exit(0);
+  }
   while (!["+", "-", "*", "/"].includes(operator)) {
     operator = prompt("Please choose a valid operator");
   }
@@ -34,6 +45,10 @@ function calculate(leftOperand, operator, rightOperand) {
     return leftOperand * rightOperand;
   }
   if (operator === "/") {
+    if (rightOperand === 0) {
+      console.log("You cant do this with 0.");
+      process.exit(0);
+    }
     return leftOperand / rightOperand;
   }
 }
